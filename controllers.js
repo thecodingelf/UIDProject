2f06060720c73acb08739dfca2570e6f3a5bb8f7
Jukka Koivu <c2koju00@students.oamk.fi>
2016-12-10 23:42:03 +0200

    Changes made
    changes to index and new content.

---
+++ controllers.js
@@ -0,0 +1,11 @@
+var work = {
+  name: 'Snow Shoveling',
+  payout: 20.00,
+  description: 'I want someone to clean my yard from the snow because I am currently busy work.'
+};
+
+    app.controller('WorkController', function($scope){
+
+      this.product = work;
+
+    });
