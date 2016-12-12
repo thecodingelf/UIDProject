d0793aaffb17f5efd534ae39e1aeb4bcd8dbaa70
Jukka Koivu <c2koju00@students.oamk.fi>
2016-12-12 19:47:42 +0200

    changes to checkwork

--- controllers.js
+++ controllers.js
@@ -1,7 +1,8 @@
 var work = {
-  name: 'Snow Shoveling',
-  payout: 20.00,
-  description: 'I want someone to clean my yard from the snow because I am currently busy work.'
+  title: 'Snow Shoveling',
+  name: 'Lisa Håls',
+  description: 'I want someone to clean my yard from the snow because I am currently busy work.',
+  payout: 20.00
 };
 
     app.controller('WorkController', function($scope){
---
+++ joblist.js
@@ -0,0 +1,12 @@
+[
+  {
+    name: 'Snow Shoveling',
+    payout: 20.00,
+    description: 'I want someone to clean my yard from the snow because I am currently busy work.'
+  },
+  {
+    name: 'Snow Cleaning',
+    payout: 15.00,
+    description: 'I want someone to clean my yard from the snow because I am currently busy work.'
+  }
+]
