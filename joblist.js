3e6d5ca5d3097417b52491a7ac0fa74ea6fac5b1
LAPTOP-4QV3I7GN\sst_h <t4tish00@students.oamk.fi>
2016-12-13 19:49:14 +0200

    joblist

--- joblist.js
+++ joblist.js
@@ -1,12 +1,18 @@
 [
   {
-    name: 'Snow Shoveling',
-    payout: 20.00,
-    description: 'I want someone to clean my yard from the snow because I am currently busy work.'
+    "title": 'Snow Shoveling',
+    "name" : "Rita rita",
+    "phone_number" : "46464640",
+    "location": "Oulu",
+    "description": 'I want someone to clean my yard from the snow because I am currently busy work.',
+    "payment" :'10'
   },
   {
-    name: 'Snow Cleaning',
-    payout: 15.00,
-    description: 'I want someone to clean my yard from the snow because I am currently busy work.'
+    "title": 'Paper work',
+    "name" : "Rita rita",
+    "phone_number" : "46464640",
+    "location": "Oulu",
+    "description": 'I want someone to currently busy work.',
+    "payment" :'10'
   }
 ]
